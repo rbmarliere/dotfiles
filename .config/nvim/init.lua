60f7d4b752d2e37db260c7632609b620c8a602d1
Ricardo Marliere <rbmarliere@gmail.com>
2022-08-26 09:46:24 -0300

    2022 config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,31 @@
+require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use 'morhetz/gruvbox'
+    use 'jamessan/vim-gnupg'
+    use 'nvim-lua/plenary.nvim'
+    use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+    use { 'nvim-treesitter/nvim-treesitter', run =  ':TSUpdate' }
+    use { 'stsewd/isort.nvim', run = ':UpdateRemotePlugins' }
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-vsnip'
+    use 'hrsh7th/vim-vsnip'
+end)
+
+require('nvim-cmp-config')
+require('nvim-treesitter-config')
+
+vim.cmd[[ source ~/.vim/vimrc ]]
+vim.cmd[[ colorscheme gruvbox ]]
+vim.o.background = 'dark'
+
+vim.api.nvim_set_keymap('n', '<C-p>', ':Telescope find_files<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>fg', ':Telescope live_grep<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>ff', ':lua vim.lsp.buf.format()<CR>:Isort<CR>', { silent = true })
+vim.cmd[[iabbrev pubd __import__('pudb').set_trace()]]
+vim.cmd[[iabbrev debug import pdb; import rlcompleter; pdb.Pdb.complete=rlcompleter.Completer(locals()).complete; pdb.set_trace()]]
+
---
+++ .config/nvim/lua/nvim-cmp-config.lua
@@ -0,0 +1,58 @@
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+
+local cmp = require'cmp'
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif vim.fn["vsnip#available"](1) == 1 then
+                feedkey("<Plug>(vsnip-expand-or-jump)", "")
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function()
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+                feedkey("<Plug>(vsnip-jump-prev)", "")
+            end
+        end, { "i", "s" }),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' },
+    }, {
+        { name = 'buffer' },
+    })
+})
+
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    })
+})
+
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+require('lspconfig')['pylsp'].setup {
+    capabilities = capabilities
+}
+
