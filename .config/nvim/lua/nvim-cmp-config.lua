2484cabdca2a5cec31ba0cf61bcb50826500d42e
Ricardo Marliere <rbmarliere@gmail.com>
2022-08-27 09:41:59 -0300

    fixes

--- .config/nvim/lua/nvim-cmp-config.lua
+++ .config/nvim/lua/nvim-cmp-config.lua
@@ -19,8 +19,6 @@ cmp.setup({
                 cmp.select_next_item()
             elseif vim.fn["vsnip#available"](1) == 1 then
                 feedkey("<Plug>(vsnip-expand-or-jump)", "")
-            elseif has_words_before() then
-                cmp.complete()
             else
                 fallback()
             end
