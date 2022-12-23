6a6b9c8817beb14fc4449dc7c993845423f994aa
Ricardo Marliere <rbmarliere@gmail.com>
2022-12-23 12:27:59 -0300

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,11 @@
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-rm -r ~/.config/{i3,nvim,compton.conf}
+rm -r ~/.config/{i3,nvim}
 rm ~/{.bashrc,.gitconfig,.inputrc,.tmux.conf}
 
 ln -s "$(pwd)/nvim" ~/.config/nvim
 ln -s "$(pwd)/i3" ~/.config/i3
-ln -s "$(pwd)/compton.conf" ~/.config/compton.conf
 ln -s "$(pwd)/.bashrc" ~/.bashrc
 ln -s "$(pwd)/.gitconfig" ~/.gitconfig
 ln -s "$(pwd)/.inputrc" ~/.inputrc
