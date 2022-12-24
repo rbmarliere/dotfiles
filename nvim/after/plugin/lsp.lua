1bb89f4319e6e31d71ca98e9b6dfaa750b80e162
Ricardo Marliere <rbmarliere@gmail.com>
2022-12-24 15:55:47 -0300

    format + organize imports on save (python)

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,45 +1,81 @@
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
+local lsp = require("lsp-zero")
+lsp.preset("recommended")
 
-local cmp = require('cmp')
+local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ['<C-Space>'] = cmp.mapping.complete()
+  ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+  ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+  ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
 })
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
+cmp_mappings["<Tab>"] = nil
+cmp_mappings["<S-Tab>"] = nil
 lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+  mapping = cmp_mappings,
 })
 
 lsp.on_attach(function(client, bufnr)
   local opts = { noremap = true, silent = true, buffer = bufnr }
-  vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, opts)
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-  vim.keymap.set('n', '<Leader>q', vim.diagnostic.setloclist, opts)
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-  -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-  vim.keymap.set('n', '<Leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-  vim.keymap.set('n', '<Leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-  vim.keymap.set('n', '<Leader>wl', function()
+  vim.keymap.set("n", "<Leader>e", vim.diagnostic.open_float, opts)
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+  vim.keymap.set("n", "<Leader>q", vim.diagnostic.setloclist, opts)
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+  -- vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+  vim.keymap.set("n", "<Leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+  vim.keymap.set("n", "<Leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+  vim.keymap.set("n", "<Leader>wl", function()
     print(vim.inspect(vim.lsp.buf.list_workLeader_folders()))
   end, opts)
-  vim.keymap.set('n', '<Leader>D', vim.lsp.buf.type_definition, opts)
-  vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, opts)
-  vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, opts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-  vim.keymap.set('n', '<Leader>f', function() vim.lsp.buf.format { async = true } end, opts)
+  vim.keymap.set("n", "<Leader>D", vim.lsp.buf.type_definition, opts)
+  vim.keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, opts)
+  vim.keymap.set("n", "<Leader>ca", vim.lsp.buf.code_action, opts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+  vim.keymap.set("n", "<Leader>f", function()
+    vim.lsp.buf.format({ async = true })
+  end, opts)
 end)
+
+lsp.configure("pylsp", {
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+      pattern = "<buffer>",
+      callback = function()
+        vim.lsp.buf.format()
+        vim.lsp.buf.code_action({
+          context = {
+            only = {
+              -- requires pylsp-rope
+              "source.organizeImports",
+            },
+          },
+          apply = true,
+        })
+      end,
+    })
+  end,
+  settings = {
+    pylsp = {
+      plugins = {
+        mccabe = {
+          threshold = 20,
+        },
+      },
+    },
+  },
+})
 
 lsp.setup()
 
 vim.diagnostic.config({
   virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
 })
--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -5,23 +5,23 @@ require("core.tabmove")
 
 local autocmd = vim.api.nvim_create_autocmd
 
-autocmd({ "filetype" }, {
+autocmd({ "FileType" }, {
   pattern = "html",
   command = [[setlocal tabstop=2 softtabstop=2 shiftwidth=2]]
 })
-autocmd({ "filetype" }, {
+autocmd({ "FileType" }, {
   pattern = "typescript",
   command = [[setlocal tabstop=2 softtabstop=2 shiftwidth=2]]
 })
-autocmd({ "filetype" }, {
+autocmd({ "FileType" }, {
   pattern = "lua",
   command = [[setlocal tabstop=2 softtabstop=2 shiftwidth=2]]
 })
-autocmd({ "filetype" }, {
+autocmd({ "FileType" }, {
   pattern = "netrw",
   command = [[nnoremap <buffer> <Space> :Ntree<CR>]]
 })
-autocmd({ "filetype" }, {
+autocmd({ "FileType" }, {
   pattern = "netrw",
   command = [[nnoremap <buffer> <C-LeftMouse> :Ntree<CR>]]
 })
