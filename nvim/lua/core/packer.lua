d6c09a389b80e7671a0c5af09c81cc5b9b56192f
Ricardo Marliere <rbmarliere@gmail.com>
2022-12-28 10:04:15 -0300

    nvim: code format

--- nvim/lua/core/packer.lua
+++ nvim/lua/core/packer.lua
@@ -1,48 +1,49 @@
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+  use("wbthomason/packer.nvim")
 
-  use('folke/which-key.nvim')
-  use('folke/zen-mode.nvim')
-  use('github/copilot.vim')
-  use('jamessan/vim-gnupg')
-  use('lukas-reineke/indent-blankline.nvim')
-  use('mbbill/undotree')
-  use('mfussenegger/nvim-dap')
-  use('p00f/nvim-ts-rainbow')
-  use('rcarriga/nvim-dap-ui')
-  use('sainnhe/gruvbox-material')
-  use('tpope/vim-commentary')
-  use('tpope/vim-fugitive')
-  use('tpope/vim-surround')
+  use("folke/which-key.nvim")
+  use("folke/zen-mode.nvim")
+  use("github/copilot.vim")
+  use("jamessan/vim-gnupg")
+  use("lukas-reineke/indent-blankline.nvim")
+  use("mbbill/undotree")
+  use("mfussenegger/nvim-dap")
+  use("p00f/nvim-ts-rainbow")
+  use("rcarriga/nvim-dap-ui")
+  use("sainnhe/gruvbox-material")
+  use("tpope/vim-commentary")
+  use("tpope/vim-fugitive")
+  use("tpope/vim-surround")
 
-  use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
-  use('nvim-treesitter/nvim-treesitter-context')
+  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+  use("nvim-treesitter/nvim-treesitter-context")
 
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+  use({
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.0",
     -- or                            , branch = '0.1.x',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  }
+    requires = { { "nvim-lua/plenary.nvim" } },
+  })
 
-  use {
-    'VonHeikemen/lsp-zero.nvim',
+  use({
+    "VonHeikemen/lsp-zero.nvim",
     requires = {
       -- LSP Support
-      { 'neovim/nvim-lspconfig' },
-      { 'williamboman/mason.nvim' },
-      { 'williamboman/mason-lspconfig.nvim' },
+      { "neovim/nvim-lspconfig" },
+      { "williamboman/mason.nvim" },
+      { "williamboman/mason-lspconfig.nvim" },
       -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-path' },
-      { 'saadparwaiz1/cmp_luasnip' },
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'hrsh7th/cmp-nvim-lua' },
+      { "hrsh7th/nvim-cmp" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "saadparwaiz1/cmp_luasnip" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-nvim-lua" },
       -- Snippets
-      { 'L3MON4D3/LuaSnip' },
-      { 'rafamadriz/friendly-snippets' },
-    }
-  }
+      { "L3MON4D3/LuaSnip" },
+      { "rafamadriz/friendly-snippets" },
+    },
+  })
 
   use({
     "jose-elias-alvarez/null-ls.nvim",
@@ -53,10 +54,9 @@ return require('packer').startup(function(use)
   })
 
   use({
-    'lewis6991/gitsigns.nvim',
+    "lewis6991/gitsigns.nvim",
     config = function()
-      require('gitsigns').setup()
-    end
+      require("gitsigns").setup()
+    end,
   })
-
 end)
