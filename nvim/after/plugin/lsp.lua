06fea3d812315a2ff3978176a738e02fabcb745e
Ricardo Marliere <rbmarliere@gmail.com>
2023-01-04 10:26:25 -0300

    nvim: keep lsp config simple

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -58,8 +58,24 @@ local on_attach = function(client, bufnr)
 end
 
 local lsp = require("lsp-zero")
-lsp.preset("recommended")
+-- lsp.preset("recommended")
+lsp.set_preferences({
+  suggest_lsp_servers = true,
+  setup_servers_on_start = true,
+  set_lsp_keymaps = false,
+  configure_diagnostics = true,
+  cmp_capabilities = true,
+  manage_nvim_cmp = true,
+  call_servers = "local",
+  sign_icons = {
+    error = "✘",
+    warn = "▲",
+    hint = "⚑",
+    info = "",
+  },
+})
 lsp.on_attach(on_attach)
+
 lsp.configure("pylsp", {
   -- organizeImports requires pylsp-rope
   on_attach = function(client, bufnr) end,
@@ -72,20 +88,6 @@ lsp.configure("pylsp", {
       },
     },
   },
-})
-
-local cmp = require("cmp")
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-  ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-  ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
-})
-cmp_mappings["<Tab>"] = nil
-cmp_mappings["<S-Tab>"] = nil
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings,
 })
 
 lsp.setup()
