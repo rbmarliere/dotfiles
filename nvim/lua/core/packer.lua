bf0449c5b7a234259273287c4b79547e6aa5dda0
Ricardo Marliere <rbmarliere@gmail.com>
2023-01-04 21:09:17 -0300

    more sources

--- nvim/lua/core/packer.lua
+++ nvim/lua/core/packer.lua
@@ -50,5 +50,7 @@ return require("packer").startup(function(use)
   use("hrsh7th/cmp-nvim-lua")
   use("hrsh7th/cmp-vsnip")
   use("hrsh7th/vim-vsnip")
+  use("hrsh7th/cmp-nvim-lsp-signature-help")
+  use("hrsh7th/cmp-nvim-lsp-document-symbol")
 
 end)
