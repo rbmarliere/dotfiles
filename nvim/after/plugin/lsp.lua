f0dcf50b89d41af06aba94c6049f7ae0d963690b
Ricardo Marliere <rbmarliere@gmail.com>
2023-01-06 15:24:44 -0300

    better dap config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -128,7 +128,34 @@ if local_rc ~= nil then
   for _, project_path in ipairs(vim.fn.split(local_rc, ":")) do
     vim.api.nvim_create_autocmd({ "BufReadPost" }, {
       pattern = project_path .. "/**",
-      command = vim.cmd("source " .. project_path .. "/.exrc"),
+      command = vim.cmd("source " .. project_path .. "/init.lua"),
     })
   end
 end
+
+-- specific neovim config
+require("lspconfig").sumneko_lua.setup({
+  -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/sumneko_lua.lua
+  on_attach = LSPAttach,
+  capabilities = LSPCapabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you"re using (most likely LuaJIT in the case of Neovim)
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { "vim" },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+})
