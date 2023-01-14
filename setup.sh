bf9a43efe5dbadd2dff43d88965adf80694e67d2
Ricardo Marliere <rbmarliere@gmail.com>
2023-01-13 21:15:33 -0300

    bump

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+mkdir -p ~/.config
+
 rm -r ~/.config/{i3,nvim}
 rm ~/{.bashrc,.gitconfig,.inputrc,.tmux.conf}
 
