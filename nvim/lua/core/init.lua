5dffe73ad1b3b64bbd8ad6457b5cb8f6be231632
Ricardo Marliere <rbmarliere@gmail.com>
2023-03-12 12:06:38 -0300

    nvim: use after/ftplugin instead of autocmd

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -2,20 +2,3 @@ require("core.map")
 require("core.packer")
 require("core.set")
 require("core.tabmove")
-
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "html",
-  command = [[setlocal tabstop=2 softtabstop=2 shiftwidth=2]],
-})
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "typescript",
-  command = [[setlocal tabstop=2 softtabstop=2 shiftwidth=2]],
-})
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "lua",
-  command = [[setlocal tabstop=2 softtabstop=2 shiftwidth=2]],
-})
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "qf",
-  command = [[wincmd J]],
-})
