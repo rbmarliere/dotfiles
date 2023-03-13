228252770338b6901ff55b8af51eeeb9a12e460c
Ricardo Marliere <rbmarliere@gmail.com>
2023-03-13 12:45:12 -0300

    nvim: fix fugitive open func

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,6 +1,19 @@
+local last_tab = 0
+
 local call_fugitive = function()
-  vim.cmd([[tab Git]])
-  vim.cmd([[tabm 0]])
+  if vim.bo.filetype == "fugitive" then
+    vim.cmd([[tabn ]] .. last_tab)
+  else
+    last_tab = vim.fn.tabpagenr()
+
+    local first_tab_buf = vim.fn.tabpagebuflist(1)[1]
+    if vim.fn.getbufvar(first_tab_buf, "&filetype") ~= "fugitive" then
+      last_tab = last_tab + 1
+    end
+
+    vim.cmd([[tab Git]])
+    vim.cmd([[tabm 0]])
+  end
 end
 
 vim.keymap.set("n", "<Leader>g", call_fugitive)
