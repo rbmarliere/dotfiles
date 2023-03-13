2617de1912cefcf939536039f87d71e8d2ca4a9e
Ricardo Marliere <rbmarliere@gmail.com>
2023-03-13 12:45:46 -0300

    nvim: dap ui

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -1,24 +1,68 @@
+require("dapui").setup({
+  layouts = { {
+    elements = { {
+      id = "breakpoints",
+      size = 0.10
+    }, {
+      id = "stacks",
+      size = 0.30
+    }, {
+      id = "watches",
+      size = 0.20
+    }, {
+      id = "scopes",
+      size = 0.40
+    } },
+    position = "left",
+    size = 60
+  }, {
+    elements = { {
+      id = "console",
+      size = 0.25
+    }, {
+      id = "repl",
+      size = 0.75
+    } },
+    position = "right",
+    size = 170
+  } },
+})
+
+local dapui_opened = false
+
 local toggle_ui = function()
   local bufname = vim.fn.bufname()
-
-  if bufname == '' then
+  if bufname == "" then
     return
   end
 
-  local curpos = vim.fn.getcurpos()
-  vim.cmd.tabedit(bufname)
-  vim.fn.setpos('.', curpos)
+  local dap_repl = vim.fn.bufnr("[dap-repl]")
+  local repl_win = vim.fn.win_findbuf(dap_repl)[1]
+  if repl_win == nil then
+    -- if dap repl is not in a window, reset the flag
+    dapui_opened = false
+  end
 
-  local widgets = require("dap.ui.widgets")
-  local dap = require("dap")
+  if dapui_opened then
+    return
+  end
 
-  widgets.sidebar(widgets.scopes, { width = 90 }).open()
+  if vim.fn.tabpagebuflist()[2] == nil then
+    require("dapui").open()
+    dapui_opened = true
+  else
+    -- if current tab has more than one buffer, open dap ui in a new tab
+    local curpos = vim.fn.getcurpos()
+    vim.cmd.tabedit(bufname)
+    vim.fn.setpos(".", curpos)
 
-  dap.repl.close()
-  dap.repl.open({ width = 150 }, "vsplit")
+    require("dapui").open()
+    dapui_opened = true
+  end
 end
 
 local opts = { silent = true }
+
 vim.keymap.set("n", "<C-x>", toggle_ui, opts)
 vim.keymap.set("n", "<F10>", ":lua require'dap'.step_over()<CR>", opts)
 vim.keymap.set("n", "<F11>", ":lua require'dap'.step_into()<CR>", opts)
