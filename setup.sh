3b15ff47e20cd22c716c40a974411434359ebb09
Ricardo Marliere <rbmarliere@gmail.com>
2023-03-14 12:41:39 -0300

    bump

--- setup.sh
+++ setup.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-#
+mkdir -p "$HOME/.config"
+
 packer="$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
 if [ ! -d "$packer" ]; then
     git clone --depth 1 https://github.com/wbthomason/packer.nvim "$packer"
 fi
 
 files=(
-    .bashrc 
+    .bashrc
     .config/alacritty
-    .config/i3 
-    .config/nvim 
-    .gitconfig 
-    .inputrc 
+    .config/i3
+    .config/nvim
+    .gitconfig
+    .inputrc
     .tmux.conf
 )
 
-mkdir -p "$HOME/.config"
-
 for file in "${files[@]}"; do
-    rm -rf "${HOME:?}/$file"
-    ln -s "$(pwd)/$file" "$HOME/$file"
+    ln -s -f -T "$(pwd)/$file" "$HOME/$file"
 done
