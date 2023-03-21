4c0dc5a104cf2c1a8288a0c1b2bcd4bd01713f22
Ricardo Marliere <rbmarliere@gmail.com>
2023-03-21 07:08:53 -0300

    bump

--- .config/nvim/after/plugin/dap.lua
+++ .config/nvim/after/plugin/dap.lua
@@ -21,11 +21,11 @@ require("dapui").setup({
     size = 65
   }, {
     elements = { {
-      id = "console",
-      size = 0.3
-    }, {
       id = "repl",
-      size = 0.7
+      size = 1
+      -- }, {
+      --   id = "repl",
+      --   size = 0.7
     } },
     position = "right",
     size = 150
@@ -77,10 +77,10 @@ vim.keymap.set("n", "<F10>", ":lua require'dap'.step_over()<CR>", opts)
 vim.keymap.set("n", "<F11>", ":lua require'dap'.step_into()<CR>", opts)
 vim.keymap.set("n", "<F12>", ":lua require'dap'.step_out()<CR>", opts)
 vim.keymap.set("n", "<F4>", ":lua require'dap'.close()<CR>", opts)
-vim.keymap.set("n", "<F5>", run_dap, opts)
+vim.keymap.set("n", "<F5>", ":sil! write | lua require'dap'.continue()<CR>", opts)
 vim.keymap.set("n", "<Leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('condition: '))<CR>", opts)
 vim.keymap.set("n", "<Leader>b", ":lua require'dap'.toggle_breakpoint()<CR>", opts)
-vim.keymap.set("n", "<Leader>dl", ":lua require'dap'.run_last()<CR>", opts)
+vim.keymap.set("n", "<Leader>dl", ":sil! write | lua require'dap'.run_last()<CR>", opts)
 vim.keymap.set("n", "<Leader>lb", ":lua require'dap'.list_breakpoints()<CR>:copen<CR>", opts)
 vim.keymap.set("n", "<Leader>lp", ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('message: '))<CR>", opts)
 vim.keymap.set("n", "<Leader>ls", function()
