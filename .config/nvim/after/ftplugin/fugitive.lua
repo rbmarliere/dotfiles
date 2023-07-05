4f5d9cad98952b7fd76122e734c6e22a69571e12
Ricardo Marliere <rbmarliere@gmail.com>
2023-07-05 13:29:23 -0300

    fugitive map

--- .config/nvim/after/ftplugin/fugitive.lua
+++ .config/nvim/after/ftplugin/fugitive.lua
@@ -2,5 +2,6 @@ local opts = { noremap = true }
 
 vim.keymap.set("n", "<Leader>sa", ":Git stash apply<CR>", opts)
 vim.keymap.set("n", "<Leader>sx", ":Git stash<CR>", opts)
-vim.keymap.set("n", "<Leader>pl", ":Git pull<CR>", opts)
+vim.keymap.set("n", "<Leader>pl", ":Git pull --rebase<CR>", opts)
 vim.keymap.set("n", "<Leader>px", ":Git push<CR>", opts)
+vim.keymap.set("n", "<Leader>g", "<C-w>q", { noremap = true, buffer = true })
