c4e4a90104e253cd8b5284f17931c12544bd3e18
Ricardo Marliere <rbmarliere@gmail.com>
2023-07-31 15:47:47 -0300

    bump

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -16,7 +16,7 @@ telescope.setup({
 	},
 	pickers = {
 		find_files = {
-			find_command = { "rg", "--files", "--hidden", "--glob", "!.git" },
+			find_command = { "rg", "--files", "--hidden", "--glob", "!.git", "--no-ignore-vcs" },
 		},
 	},
 	extensions = {
@@ -46,6 +46,6 @@ telescope.setup({
 local builtin = require("telescope.builtin")
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<C-Space>", builtin.lsp_document_symbols, opts)
-vim.keymap.set("n", "<C-p>", builtin.find_files, opts)
-vim.keymap.set("n", "<Leader>pf", builtin.git_files, opts)
-vim.keymap.set("n", "<Leader>pg", telescope.extensions.live_grep_args.live_grep_args, opts)
+vim.keymap.set("n", "<C-p>", builtin.git_files, opts)
+vim.keymap.set("n", "<Leader>ff", builtin.find_files, opts)
+vim.keymap.set("n", "<Leader>gr", telescope.extensions.live_grep_args.live_grep_args, opts)
