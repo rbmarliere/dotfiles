818e5c63f8f1b2a21c1c29a9588e983c00d1385f
Ricardo B. Marliere <ricardo@marliere.net>
2024-01-30 10:37:44 -0300

    nvim: codespell linter

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -2,6 +2,15 @@ return {
 	"mfussenegger/nvim-lint",
 	config = function()
 		local lint = require("lint")
+		lint.linters.codespell = {
+			cmd = "codespell --config ~/.codespellrc --config ../.codespellrc",
+			stdin = false,
+			ignore_exitcode = true,
+			parser = require("lint.parser").from_errorformat(
+				"%f:%l:%m",
+				{ severity = vim.diagnostic.severity.INFO, source = "codespell" }
+			),
+		}
 		lint.linters_by_ft = {
 			c = { "checkpatch" },
 			markdown = { "vale" },
