e435ebea32a32ba3ae695e83160970e2860fa2fe
Ricardo B. Marliere <ricardo@marliere.net>
2024-01-30 10:36:26 -0300

    nvim: better zen config

--- .config/nvim/lua/plugins/zen.lua
+++ .config/nvim/lua/plugins/zen.lua
@@ -1,11 +1,23 @@
+ColorColumn = nil
+
 return {
-  "folke/zen-mode.nvim",
-  opts = {
+	"folke/zen-mode.nvim",
+	opts = {
 		window = {
 			width = 100,
-		}
-  },
+		},
+		plugins = {
+			tmux = { enabled = true },
+		},
+		on_open = function(_)
+			ColorColumn = vim.opt_local.colorcolumn
+			vim.opt_local.colorcolumn = ""
+		end,
+		on_close = function(_)
+			vim.opt_local.colorcolumn = ColorColumn
+		end,
+	},
 	keys = {
 		{ "<leader>z", ":ZenMode<CR>" },
-	}
+	},
 }
