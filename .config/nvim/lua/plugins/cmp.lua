11c48cc182ee102986e43685306dbb1b11246333
Ricardo B. Marliere <ricardo@marliere.net>
2024-02-08 10:58:48 -0300

    nvim: cmp: make luasnip integration better
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,3 +1,9 @@
+local has_words_before = function()
+	unpack = unpack or table.unpack
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 return {
 	{
 		"hrsh7th/nvim-cmp",
@@ -14,12 +20,13 @@ return {
 			{ "tzachar/cmp-fuzzy-buffer", dependencies = { "tzachar/fuzzy.nvim" } },
 		},
 		config = function()
+			local ls = require("luasnip")
 			local cmp = require("cmp")
 			cmp.setup({
 				preselect = cmp.PreselectMode.None,
 				snippet = {
 					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
+						ls.lsp_expand(args.body)
 					end,
 				},
 				window = {
@@ -33,6 +40,28 @@ return {
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = false }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+						-- that way you will only jump inside the snippet region
+						elseif ls.expand_or_jumpable() then
+							ls.expand_or_jump()
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif ls.jumpable(-1) then
+							ls.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "luasnip" },
