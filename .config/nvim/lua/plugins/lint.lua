3f5a5ac298bcc1d38351b2f543157037b00d4f71
Ricardo B. Marliere <ricardo@marliere.net>
2024-02-10 16:29:36 -0300

    nvim: basic klint setup
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -2,6 +2,21 @@ return {
 	"mfussenegger/nvim-lint",
 	config = function()
 		local lint = require("lint")
+		lint.linters.klint = {
+			name = "klint",
+			cmd = "rustup",
+			args = {
+				"run",
+				"1.75.0",
+				"klint",
+				"--error-format",
+				"short",
+			},
+			stdin = false,
+			ignore_exitcode = true,
+			stream = "stderr",
+			parser = require("lint.parser").from_errorformat("%f:%l:%c: %m")
+		}
 		lint.linters.codespell.args = {
 			"--config",
 			"~/.codespellrc",
@@ -13,6 +28,7 @@ return {
 		lint.linters_by_ft = {
 			c = { "checkpatch" },
 			markdown = { "vale" },
+			-- rust = { "klint" },
 		}
 		vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave", "TextChanged" }, {
 			callback = function()
