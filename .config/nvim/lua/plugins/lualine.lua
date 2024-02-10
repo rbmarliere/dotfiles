073e612d079f330cb1c6622055a83e8455555672
Ricardo B. Marliere <ricardo@marliere.net>
2024-02-10 15:02:59 -0300

    nvim: lualine: add relative paths to filenames
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -67,7 +67,12 @@ return {
 					},
 				},
 				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "filename" },
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
 				lualine_x = { "encoding" }, --"fileformat", "filetype" },
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
@@ -75,7 +80,12 @@ return {
 			inactive_sections = {
 				lualine_a = {},
 				lualine_b = {},
-				lualine_c = { "filename" },
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
 				lualine_x = { "location" },
 				lualine_y = {},
 				lualine_z = {},
