4eae7f0e407d23f6921b1302b4de72c4a90e39dc
Ricardo B. Marliere <ricardo@marliere.net>
2024-02-10 10:46:53 -0300

    nvim: telescope: use default picker config
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -8,67 +8,70 @@ return {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
-	opts = {
-		defaults = {
-			border = true,
-			borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-			sorting_strategy = "ascending",
-		},
-		pickers = {
-			find_files = {
-				find_command = { "rg", "--files", "--hidden", "--glob", "!.git", "--no-ignore-vcs" },
-			},
-			lsp_workspace_symbols = {
-				fname_width = 60,
-				symbol_width = 50,
-				symbol_type_width = 15,
-			},
-			lsp_dynamic_workspace_symbols = {
-				fname_width = 60,
-				symbol_width = 50,
-				symbol_type_width = 15,
-			},
-			man_pages = {
-				sections = { "1", "2", "3", "4", "5", "6", "7", "8", "9" },
-			},
-		},
-		extensions = {
-			live_grep_args = {
-				vimgrep_arguments = {
-					"rg",
-					"--pcre2",
-					"--glob",
-					"!.git",
-					"--hidden",
-					"--color=never",
-					"--no-heading",
-					"--with-filename",
-					"--line-number",
-					"--column",
-					"--smart-case",
-				},
-				mappings = {
-					i = {
-						["<C-k>"] = function(prompt_bufnr)
-							require("telescope-live-grep-args.actions").quote_prompt()(prompt_bufnr)
-						end,
-						["<C-j>"] = function(prompt_bufnr)
-							require("telescope-live-grep-args.actions").quote_prompt({ postfix = " --iglob " })(
-								prompt_bufnr
-							)
-						end,
-					},
-				},
-			},
-		},
-	},
 	keys = {
 		{ "<C-Space>", ":Telescope find_pickers<CR>" },
 		{ "<C-p>", ":Telescope git_files<CR>" },
 	},
-	config = function(_, opts)
+	config = function()
 		local telescope = require("telescope")
+		local builtin = require("telescope.builtin")
+
+		local picker_config = {}
+		for b, _ in pairs(builtin) do
+			picker_config[b] = {
+				fname_width = 60,
+				symbol_width = 50,
+				symbol_type_width = 15,
+			}
+		end
+
+		local opts = {
+			defaults = {
+				border = true,
+				borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+				sorting_strategy = "ascending",
+			},
+			pickers = vim.tbl_extend("force", picker_config, {
+				find_files = {
+					find_command = { "rg", "--files", "--hidden", "--glob", "!.git", "--no-ignore-vcs" },
+				},
+				man_pages = {
+					sections = { "1", "2", "3", "4", "5", "6", "7", "8", "9" },
+				},
+			}),
+			extensions = {
+				live_grep_args = {
+					vimgrep_arguments = {
+						"rg",
+						"--pcre2",
+						"--glob",
+						"!.git",
+						"--hidden",
+						"--color=never",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+						"--smart-case",
+					},
+					mappings = {
+						i = {
+							["<C-k>"] = function(prompt_bufnr)
+								require("telescope-live-grep-args.actions").quote_prompt()(prompt_bufnr)
+							end,
+							["<C-j>"] = function(prompt_bufnr)
+								require("telescope-live-grep-args.actions").quote_prompt({ postfix = " --iglob " })(
+									prompt_bufnr
+								)
+							end,
+						},
+					},
+				},
+			},
+		}
+
 		telescope.setup(opts)
+
 		telescope.load_extension("live_grep_args")
 		telescope.load_extension("fzf")
 		telescope.load_extension("find_pickers")
