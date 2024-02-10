a446078f530ce987a2f92450feb5163e2353d27d
Ricardo B. Marliere <ricardo@marliere.net>
2024-02-10 11:02:09 -0300

    nvim: use default laststatus for man file type
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>

--- .config/nvim/after/ftplugin/man.lua
+++ .config/nvim/after/ftplugin/man.lua
@@ -2,7 +2,6 @@ local opts = { silent = true, buffer = true, nowait = true }
 
 vim.keymap.set("n", "q", ":lclose<CR><C-W>q", opts)
 
-vim.opt_local.laststatus = 0
 vim.opt_local.modifiable = false
 vim.opt_local.modified = false
 vim.opt_local.buftype = "nofile"
