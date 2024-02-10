5a20926105b74a0a866767b5ca7559e946e36946
Ricardo B. Marliere <ricardo@marliere.net>
2024-02-10 11:07:49 -0300

    nvim: snippets
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>

--- .config/nvim/lua/snippets/mail.lua
+++ .config/nvim/lua/snippets/mail.lua
@@ -1,5 +1,6 @@
 return {
-	s("Thanks", t({"Thanks for reviewing,", "-\tRicardo.", "", ""})),
+	s("Thanks", t({"Thanks,", "-\tRicardo.", "", ""})),
+	s("Thanks for reviewing", t({"Thanks for reviewing,", "-\tRicardo.", "", ""})),
 	s("Thank you", t({"Thank you,", "-\tRicardo.", "", ""})),
 	s("Regards", t({"Best regards,", "-\tRicardo.", "", ""})),
 	s("All the best", t({"All the best,", "-\tRicardo.", "", ""})),
