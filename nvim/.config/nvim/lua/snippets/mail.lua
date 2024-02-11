ed831cd7ad29c910d355c811830d0137707a5491
Ricardo B. Marliere <ricardo@marliere.net>
2024-02-11 07:47:42 -0300

    nvim: mail snippets
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>

--- nvim/.config/nvim/lua/snippets/mail.lua
+++ nvim/.config/nvim/lua/snippets/mail.lua
@@ -1,7 +1,14 @@
 return {
-	s("Thanks", t({"Thanks,", "-\tRicardo.", "", ""})),
-	s("Thanks for reviewing", t({"Thanks for reviewing,", "-\tRicardo.", "", ""})),
-	s("Thank you", t({"Thank you,", "-\tRicardo.", "", ""})),
-	s("Regards", t({"Best regards,", "-\tRicardo.", "", ""})),
-	s("All the best", t({"All the best,", "-\tRicardo.", "", ""})),
+	s("Thanks", t({ "Thanks,", "-\tRicardo.", "", "" })),
+	s("Thanks for reviewing", t({ "Thanks for reviewing,", "-\tRicardo.", "", "" })),
+	s("Thank you", t({ "Thank you,", "-\tRicardo.", "", "" })),
+	s("Regards", t({ "Best regards,", "-\tRicardo.", "", "" })),
+	s("All the best", t({ "All the best,", "-\tRicardo.", "", "" })),
+	s(
+		"Sign",
+		t({
+			"Ricardo B. Marliere         https://marliere.net/",
+			"030A 8E9E 424E E3C0 6557 87E1 C90B 8A7C 6386 58A6",
+		})
+	),
 }
