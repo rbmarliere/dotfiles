6cc3eb5d55e8fe8928eb06a8a70a4fb872c8be6e
Ricardo B. Marliere <ricardo@marliere.net>
2024-02-12 16:31:16 -0300

    nvim: snippets
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>

--- neovim/.config/nvim/lua/snippets/gitcommit.lua
+++ neovim/.config/nvim/lua/snippets/gitcommit.lua
@@ -3,17 +3,23 @@ local function simple_restore(args, _)
 end
 
 return {
-	s("const", {
+	s("bus_type const", {
 		i(1, "subsystem"),
 		t(" make "),
 		i(2, "variable"),
 		t(" const"),
 		t({ "", "", "" }),
-		t("Now that the driver core can properly handle constant struct bus_type,"),
-		t({ "", "move the " }),
+		t('Since commit d492cc2573a0 ("driver core: device.h: make struct'),
+		t({ "", "" }),
+		t('bus_type a const *"), the driver core can properly handle constant'),
+		t({ "", "" }),
+		t("struct bus_type, move the "),
 		d(3, simple_restore, 2),
-		t(" variable to be a constant structure as well,"),
-		t({ "", "placing it into read-only memory which can not be modified at runtime." }),
+		t(" variable to be a constant"),
+		t({ "", "" }),
+		t("structure as well, placing it into read-only memory which can not be"),
+		t({ "", "" }),
+		t("modified at runtime."),
 		t({ "", "", "Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>" }),
 		t({ "", "Suggested-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>" }),
 	}),
