792a291bb49c54b872ed2a29050b12275e2e166d
Ricardo B. Marliere <ricardo@marliere.net>
2024-02-16 13:53:33 -0300

    nvim: model: add zephyr llamacpp chat
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>

--- neovim/.config/nvim/lua/plugins/model.lua
+++ neovim/.config/nvim/lua/plugins/model.lua
@@ -12,10 +12,13 @@ return {
 	keys = {
 		{ "<C-m>4", ":tab Mchat gpt4<CR>", mode = "n" },
 		{ "<C-m>g", ":tab Mchat openai<CR>", mode = "n" },
-		{ "<C-m><Space>", ":Mchat<CR>", mode = "n" },
+		{ "<C-m>z", ":tab Mchat zephyr<CR>", mode = "n" },
+		{ "<C-m><Space>", ":Mchat<CR>G", mode = "n" },
 	},
 	config = function()
 		local chats = require("model.prompts.chats")
+		local llamacpp = require("model.providers.llamacpp")
+
 		require("model").setup({
 			chats = {
 				openai = vim.tbl_deep_extend("force", chats["openai"], {
@@ -28,7 +31,27 @@ return {
 						model = "gpt-4-0125-preview",
 					},
 				}),
+				zephyr = vim.tbl_deep_extend("force", vim.deepcopy(chats["llamacpp:zephyr"]), {
+					options = {
+						-- model = "zephyr-7b-beta/ggml-model-f16.gguf",
+						model = nil, -- (nil disables autostart and expects a running server @127.0.0.1:8080
+						args = {},
+					},
+				}),
 			},
+		})
+
+		llamacpp.setup({
+			binary = "~/models/llama.cpp/server",
+			models = "~/models/",
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "mchat" },
+			callback = function()
+				-- vim.cmd.syntax("off")
+				vim.cmd.setlocal("foldmethod=manual")
+			end,
 		})
 	end,
 }
