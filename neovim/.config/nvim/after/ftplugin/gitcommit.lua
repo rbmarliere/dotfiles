1607c719cd5166731fa8e80bab00abf1d96ab457
Ricardo B. Marliere <ricardo@marliere.net>
2024-02-18 11:08:09 -0300

    nvim: wrap at 75 cols in gitcommit ft
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>

--- neovim/.config/nvim/after/ftplugin/gitcommit.lua
+++ neovim/.config/nvim/after/ftplugin/gitcommit.lua
@@ -1,6 +1,6 @@
-vim.opt_local.colorcolumn = "72"
+vim.opt_local.colorcolumn = "75"
 vim.opt_local.spell = true
-vim.opt_local.tw = 72
+vim.opt_local.tw = 75
 vim.opt_local.wrap = true
 
 vim.cmd.wincmd("T")
