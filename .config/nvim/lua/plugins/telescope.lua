3a3779ea4bcc989c2c7d11b085161705c01a155f
Ricardo B. Marliere <ricardo@marliere.net>
2024-02-20 10:50:25 -0300

    nvim: telescope: add file-browser sane defaults
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -21,6 +21,7 @@ return {
 		{ "<C-Space>", ":Telescope find_pickers<CR>" },
 		{ "<C-p>", ":Telescope git_files<CR>" },
 		{ "-", ":Telescope file_browser path=%:p:h<CR>" },
+		{ "<M-g>", ":Telescope live_grep_args<CR>" },
 	},
 	config = function()
 		local telescope = require("telescope")
@@ -28,6 +29,7 @@ return {
 		local action_state = require("telescope.actions.state")
 		local actions = require("telescope.actions")
 		local fb_utils = require("telescope._extensions.file_browser.utils")
+		local fb_actions = require("telescope._extensions.file_browser.actions")
 		local Path = require("plenary.path")
 
 		local picker_config = {}
@@ -37,6 +39,40 @@ return {
 				symbol_width = 50,
 				symbol_type_width = 15,
 			}
+		end
+
+		-- https://github.com/nvim-telescope/telescope-file-browser.nvim/wiki/Configuration-Recipes
+		local path_toggle = function(prompt_bufnr)
+			local current_picker = action_state.get_current_picker(prompt_bufnr)
+			local finder = current_picker.finder
+			local bufr_path = Path:new(vim.fn.expand("#:p"))
+			local bufr_parent_path = bufr_path:parent():absolute()
+			if finder.path ~= bufr_parent_path then
+				finder.path = bufr_parent_path
+				fb_utils.selection_callback(current_picker, bufr_path:absolute())
+			else
+				finder.path = vim.loop.cwd()
+			end
+			fb_utils.redraw_border_title(current_picker)
+			current_picker:refresh(finder, {
+				new_prefix = fb_utils.relative_path_prefix(finder),
+				reset_prompt = true,
+				multi = current_picker._multi,
+			})
+		end
+		local grep_in_dir = function(prompt_bufnr)
+			local selections = fb_utils.get_selected_files(prompt_bufnr, false)
+			local search_dirs = vim.tbl_map(function(path)
+				return path:absolute()
+			end, selections)
+			if vim.tbl_isempty(search_dirs) then
+				local current_finder = action_state.get_current_picker(prompt_bufnr).finder
+				search_dirs = { current_finder.path }
+			end
+			actions.close(prompt_bufnr)
+			require("telescope").extensions.live_grep_args.live_grep_args({
+				search_dirs = search_dirs,
+			})
 		end
 
 		local opts = {
@@ -73,41 +109,22 @@ return {
 					},
 				},
 				file_browser = {
+					git_status = false,
+					hidden = { file_browser = true, folder_browser = true },
+					hide_parent_dir = true,
+					hijack_netrw = true,
 					mappings = {
-						["i"] = {
-							-- from https://github.com/nvim-telescope/telescope-file-browser.nvim/wiki/Configuration-Recipes
-							["<M-g>"] = function(prompt_bufnr)
-								local selections = fb_utils.get_selected_files(prompt_bufnr, false)
-								local search_dirs = vim.tbl_map(function(path)
-									return path:absolute()
-								end, selections)
-								if vim.tbl_isempty(search_dirs) then
-									local current_finder = action_state.get_current_picker(prompt_bufnr).finder
-									search_dirs = { current_finder.path }
-								end
-								actions.close(prompt_bufnr)
-								require("telescope").extensions.live_grep_args.live_grep_args({
-									search_dirs = search_dirs ,
-								})
-							end,
-							["<C-e>"] = function(prompt_bufnr)
-								local current_picker = action_state.get_current_picker(prompt_bufnr)
-								local finder = current_picker.finder
-								local bufr_path = Path:new(vim.fn.expand("#:p"))
-								local bufr_parent_path = bufr_path:parent():absolute()
-								if finder.path ~= bufr_parent_path then
-									finder.path = bufr_parent_path
-									fb_utils.selection_callback(current_picker, bufr_path:absolute())
-								else
-									finder.path = vim.loop.cwd()
-								end
-								fb_utils.redraw_border_title(current_picker)
-								current_picker:refresh(finder, {
-									new_prefix = fb_utils.relative_path_prefix(finder),
-									reset_prompt = true,
-									multi = current_picker._multi,
-								})
-							end,
+						i = {
+							["<C-t>"] = actions.select_tab, -- https://github.com/nvim-telescope/telescope-file-browser.nvim/issues/250
+							["<M-g>"] = grep_in_dir,
+							["<C-e>"] = path_toggle,
+						},
+						n = {
+							["-"] = fb_actions.goto_parent_dir,
+							t = actions.select_tab,
+							T = fb_actions.change_cwd,
+							["<M-g>"] = grep_in_dir,
+							["<C-e>"] = path_toggle,
 						},
 					},
 				},
