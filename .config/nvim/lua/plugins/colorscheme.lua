b6d543680f5bf61447fc4c17c3534b869ac37ed2
Ricardo B. Marliere <ricardo@marliere.net>
2024-02-29 11:31:02 -0300

    nvim: add unfocused transparency
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -4,14 +4,15 @@ local my_theme = {
 	WhiteSpaceBol = { bg = colors.white },
 	SignColumn = { bg = "none" },
 	Normal = { bg = "none" },
+	NormalNC = { bg = "none" },
 	NormalFloat = { bg = colors.white },
 	FloatBorder = { fg = colors.green, bg = "none" },
 	StatusLine = { fg = colors.green, bg = colors.black },
 	StatusLineNC = { fg = colors.lightgreen, bg = colors.lightgray },
 	VertSplit = { fg = colors.green, bg = "none" },
-	TabLine = { fg = colors.lightgreen, bg = colors.green },
+	TabLine = { fg = colors.lightgreen, bg = colors.lightgreen },
 	TabLineSel = { fg = colors.black, bg = colors.green },
-	TabLineFill = { fg = colors.green, bg = colors.green },
+	TabLineFill = { fg = colors.lightgreen, bg = colors.lightgreen },
 	ColorColumn = { bg = colors.lightwhite },
 	CursorColumn = { bg = colors.green },
 	CursorLine = { fg = colors.black, bg = colors.green },
@@ -29,6 +30,55 @@ local my_theme = {
 	DiagnosticVirtualTextHint = { fg = colors.cyan, bg = colors.white },
 	DiagnosticVirtualTextOk = { bg = colors.white },
 }
+
+-- helper to set current window highlight
+local set_winhightlight = function(hl)
+	local win = vim.api.nvim_get_current_win()
+	local config = vim.api.nvim_win_get_config(win)
+	if config.relative == "" then
+		-- this is a not floating window
+		vim.api.nvim_win_set_option(win, "winhighlight", hl)
+	end
+end
+-- highlight groups used to set active window bg opaque and keep others transparent
+vim.api.nvim_set_hl(0, "Transparent", {
+	bg = "none",
+})
+vim.api.nvim_set_hl(0, "Opaque", {
+	bg = colors.lighterwhite,
+})
+vim.api.nvim_set_hl(0, "WhiteBG", {
+	bg = colors.lightwhite,
+})
+vim.api.nvim_create_augroup("MyHighlights", { clear = true })
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter", "FocusGained" }, {
+	group = "MyHighlights",
+	pattern = "*",
+	callback = function()
+		set_winhightlight("Normal:Opaque,ColorColumn:WhiteBG")
+	end,
+})
+vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave", "FocusLost" }, {
+	group = "MyHighlights",
+	pattern = "*",
+	callback = function()
+		set_winhightlight("Normal:Transparent,ColorColumn:Transparent")
+	end,
+})
+
+-- integration with tmux.conf window-renamed event hook
+vim.api.nvim_create_autocmd("VimEnter", {
+	group = "MyHighlights",
+	callback = function()
+		vim.fn.system("[ -z $TMUX_PANE ] || tmux rename-window nvim")
+	end,
+})
+vim.api.nvim_create_autocmd("VimLeave", {
+	group = "MyHighlights",
+	callback = function()
+		vim.fn.system("[ -z $TMUX_PANE ] || tmux rename-window bash")
+	end,
+})
 
 return {
 	"ellisonleao/gruvbox.nvim",
