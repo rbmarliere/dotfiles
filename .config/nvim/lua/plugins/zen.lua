6c4784b79f7626d42ec1ea258afb6b1dcd6e4b60
Ricardo B. Marliere <ricardo@marliere.net>
2024-02-29 11:32:14 -0300

    nvim: zen: add transparency
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>

--- .config/nvim/lua/plugins/zen.lua
+++ .config/nvim/lua/plugins/zen.lua
@@ -8,10 +8,12 @@ return {
 		},
 		plugins = {
 			tmux = { enabled = true },
+			-- options = { laststatus = 3 },
 		},
 		on_open = function(_)
 			ColorColumn = vim.opt_local.colorcolumn
 			vim.opt_local.colorcolumn = ""
+			vim.api.nvim_win_set_option(0, "winhighlight", "Normal:Opaque")
 		end,
 		on_close = function(_)
 			vim.opt_local.colorcolumn = ColorColumn
