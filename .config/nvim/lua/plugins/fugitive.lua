00379ad2b2e45cb0869b988933b1caf3b33c8da5
Ricardo B. Marliere <ricardo@marliere.net>
2024-02-29 11:35:58 -0300

    nvim: sane fugitive default key maps
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -2,7 +2,8 @@ return {
 	"tpope/vim-fugitive",
 	lazy = false,
 	keys = {
-		{ "<Leader>g", ":tab Git<CR>" },
-		{ "<Leader>G", ":tab Git blame %<CR>" },
+		{ "<Leader>g", ":vert Git<CR>" },
+		{ "<Leader>G", ":tab Git<CR>:tabm 0<CR>" },
+		{ "<Leader><Leader>g", ":Git blame<CR>:set number relativenumber<CR>" },
 	},
 }
