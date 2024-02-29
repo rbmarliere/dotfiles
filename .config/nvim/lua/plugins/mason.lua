46437614770aba6a91c4d4734be989d765a80387
Ricardo B. Marliere <ricardo@marliere.net>
2024-02-29 11:37:56 -0300

    nvim: remove python lsp config overrides
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -20,28 +20,28 @@ return {
 							cmd = { "clangd", "--offset-encoding=utf-16" },
 						})
 					end,
-					["ruff_lsp"] = function()
-						lspconfig["ruff_lsp"].setup({
-							settings = {
-								interpreter = vim.fn.getcwd() .. "/.venv/bin/python",
-							},
-						})
-					end,
-					["pyright"] = function()
-						lspconfig["pyright"].setup({
-							settings = {
-								pyright = {
-									disableOrganizeImports = true,
-								},
-								python = {
-									pythonPath = vim.fn.getcwd() .. "/.venv/bin/python",
-									analysis = {
-										typeCheckingMode = "off",
-									},
-								},
-							},
-						})
-					end,
+					-- ["ruff_lsp"] = function()
+					-- 	lspconfig["ruff_lsp"].setup({
+					-- 		settings = {
+					-- 			interpreter = vim.fn.getcwd() .. "/.venv/bin/python",
+					-- 		},
+					-- 	})
+					-- end,
+					-- ["pyright"] = function()
+					-- 	lspconfig["pyright"].setup({
+					-- 		settings = {
+					-- 			pyright = {
+					-- 				disableOrganizeImports = true,
+					-- 			},
+					-- 			python = {
+					-- 				pythonPath = vim.fn.getcwd() .. "/.venv/bin/python",
+					-- 				analysis = {
+					-- 					typeCheckingMode = "off",
+					-- 				},
+					-- 			},
+					-- 		},
+					-- 	})
+					-- end,
 					["lua_ls"] = function()
 						lspconfig["lua_ls"].setup({
 							settings = {
@@ -55,6 +55,15 @@ return {
 					end,
 				})
 			end,
+		},
+		{
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			opts = {
+				ensure_installed = {
+					"shfmt",
+					-- TODO: add essentials
+				},
+			},
 		},
 	},
 }
