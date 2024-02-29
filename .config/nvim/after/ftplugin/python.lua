74a785b9a21b6a1e0a1334d4bb1e3a5111f2b37d
Ricardo B. Marliere <ricardo@marliere.net>
2024-02-29 11:31:41 -0300

    nvim: add python debugging snippet
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>

--- .config/nvim/after/ftplugin/python.lua
+++ .config/nvim/after/ftplugin/python.lua
@@ -3,5 +3,3 @@ vim.opt_local.expandtab = true
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.tabstop = 4
-
-vim.cmd.iabbrev("debug", "import code; code.interact(local=vars())")
---
+++ .config/nvim/lua/snippets/python.lua
@@ -0,0 +1,3 @@
+return {
+	s("debug", t("import code; code.interact(local=vars())")),
+}
