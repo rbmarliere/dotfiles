5a38ddc0456d24280e68cd29e8e7b9962c6eaad3
Ricardo B. Marliere <ricardo@marliere.net>
2024-02-29 11:39:03 -0300

    nvim: lualine: fix colors, add custom_fname, use tabline
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,33 +3,33 @@ local colors = require("utils.colors")
 local my_theme = {
 	normal = {
 		a = { bg = colors.darkgray, fg = colors.white, gui = "bold" },
-		b = { bg = colors.lightgreen, fg = colors.darkgray },
-		c = { bg = colors.green, fg = colors.black },
+		b = { bg = colors.green, fg = colors.black },
+		c = { bg = colors.lightgreen, fg = colors.darkgray },
 	},
 	insert = {
 		a = { bg = colors.purple, fg = colors.white, gui = "bold" },
-		b = { bg = colors.lightgreen, fg = colors.darkgray },
-		c = { bg = colors.green, fg = colors.black },
+		b = { bg = colors.green, fg = colors.black },
+		c = { bg = colors.lightgreen, fg = colors.darkgray },
 	},
 	visual = {
 		a = { bg = colors.red, fg = colors.white, gui = "bold" },
-		b = { bg = colors.lightgreen, fg = colors.darkgray },
-		c = { bg = colors.green, fg = colors.black },
+		b = { bg = colors.green, fg = colors.black },
+		c = { bg = colors.lightgreen, fg = colors.darkgray },
 	},
 	replace = {
 		a = { bg = colors.yellow, fg = colors.white, gui = "bold" },
-		b = { bg = colors.lightgreen, fg = colors.darkgray },
-		c = { bg = colors.green, fg = colors.black },
+		b = { bg = colors.green, fg = colors.black },
+		c = { bg = colors.lightgreen, fg = colors.darkgray },
 	},
 	command = {
 		a = { bg = colors.blue, fg = colors.black, gui = "bold" },
-		b = { bg = colors.lightgreen, fg = colors.darkgray },
-		c = { bg = colors.green, fg = colors.black },
+		b = { bg = colors.green, fg = colors.black },
+		c = { bg = colors.lightgreen, fg = colors.darkgray },
 	},
 	inactive = {
 		a = { bg = colors.lightgreen, fg = colors.inactivegray },
-		b = { bg = colors.lightgreen, fg = colors.inactivegray },
-		c = { bg = colors.lightgreen, fg = colors.inactivegray },
+		b = { bg = colors.lightergreen, fg = colors.inactivegray },
+		c = { bg = colors.lightergreen, fg = colors.inactivegray },
 	},
 }
 
@@ -38,6 +38,36 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {},
 	config = function()
+		-- https://github.com/nvim-lualine/lualine.nvim/wiki/Component-snippets#changing-filename-color-based-on--modified-status
+		local custom_fname = require("lualine.components.filename"):extend()
+		local highlight = require("lualine.highlight")
+		local default_status_colors = { saved = colors.green, modified = colors.orange }
+		function custom_fname:init(options)
+			custom_fname.super.init(self, options)
+			self.status_colors = {
+				saved = highlight.create_component_highlight_group(
+					{ bg = default_status_colors.saved },
+					"filename_status_saved",
+					self.options
+				),
+				modified = highlight.create_component_highlight_group(
+					{ bg = default_status_colors.modified },
+					"filename_status_modified",
+					self.options
+				),
+			}
+			-- if self.options.color == nil then
+			-- 	self.options.color = ""
+			-- end
+		end
+		function custom_fname:update_status()
+			local data = custom_fname.super.update_status(self)
+			data = highlight.component_format_highlight(
+				vim.bo.modified and self.status_colors.modified or self.status_colors.saved
+			) .. data
+			return data
+		end
+
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
@@ -66,31 +96,84 @@ return {
 						end,
 					},
 				},
-				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = {
+				lualine_b = {
 					{
-						"filename",
+						custom_fname,
 						path = 1,
 					},
 				},
-				lualine_x = { "encoding" }, --"fileformat", "filetype" },
-				lualine_y = { "progress" },
+				lualine_c = { "branch", "diff", "diagnostics" },
+				lualine_x = { "encoding", "fileformat" }, --"fileformat", "filetype" },
+				lualine_y = { "progress", "searchcount" },
 				lualine_z = { "location" },
 			},
 			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = {
+				lualine_a = {
 					{
-						"filename",
+						custom_fname,
 						path = 1,
 					},
 				},
-				lualine_x = { "location" },
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = { "location" },
+			},
+			tabline = {
+				lualine_a = {
+					{
+						"tabs",
+						tab_max_length = 50, -- Maximum width of each tab. The content will be shorten dynamically (example: apple/orange -> a/orange)
+						max_length = vim.o.columns, -- Maximum width of tabs component.
+						-- Note:
+						-- It can also be a function that returns
+						-- the value of `max_length` dynamically.
+						mode = 1, -- 0: Shows tab_nr
+						-- 1: Shows tab_name
+						-- 2: Shows tab_nr + tab_name
+
+						path = 1, -- 0: just shows the filename
+						-- 1: shows the relative path and shorten $HOME to ~
+						-- 2: shows the full path
+						-- 3: shows the full path and shorten $HOME to ~
+
+						-- Automatically updates active tab color to match color of other components (will be overridden if buffers_color is set)
+						use_mode_colors = false,
+
+						tabs_color = {
+							-- Same values as the general color option can be used here.
+							active = "lualine_a_normal", -- Color for active tab.
+							inactive = "lualine_b_normal", -- Color for inactive tab.
+						},
+
+						show_modified_status = true, -- Shows a symbol next to the tab name if the file has been modified.
+						symbols = {
+							modified = "[+]", -- Text to show when the file is modified.
+						},
+
+						fmt = function(name, context)
+							-- Show + if buffer is modified in tab
+							-- local buflist = vim.fn.tabpagebuflist(context.tabnr)
+							-- local winnr = vim.fn.tabpagewinnr(context.tabnr)
+							-- local bufnr = buflist[winnr]
+							-- local mod = vim.fn.getbufvar(bufnr, "&mod")
+							-- return name .. (mod == 1 and " +" or "")
+
+							if string.match(name, "^fugitive:") then
+								name = "git"
+							end
+
+							return name
+						end,
+					},
+				},
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
 				lualine_y = {},
 				lualine_z = {},
 			},
-			tabline = {},
 			winbar = {},
 			inactive_winbar = {},
 			extensions = {
