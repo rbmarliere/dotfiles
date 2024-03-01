fa6333b21f07032414a990c09ce762cb8bb569b9
Ricardo B. Marliere <ricardo@marliere.net>
2024-03-01 18:29:44 -0300

    nvim: add get_maintainer helper module
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,15 @@
+local get_maintainer = require("utils.get_maintainer")
+
 local opts = { noremap = true }
 
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", opts)
-vim.keymap.set("n", "<Leader>M", ":!get_maintainer.pl % | tee >(wl-copy -p)<CR>", opts)
 
+vim.keymap.set("n", "<Leader>M", get_maintainer.get_from_file, opts)
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "fugitive" },
+	callback = function()
+		local opts = { noremap = true, buffer = true }
+		vim.keymap.set("n", "<Leader>M", get_maintainer.get_from_cursor, opts)
+		vim.keymap.set("x", "<Leader>M", get_maintainer.get_from_range, opts)
+	end,
+})
