8b3ff038a4250b888119b7a80d723efe40be7aaa
Ricardo B. Marliere <ricardo@marliere.net>
2024-03-08 15:55:05 -0300

    nvim: fix mail snippet
    Signed-off-by: Ricardo B. Marliere <ricardo@marliere.net>

--- .config/nvim/lua/snippets/mail.lua
+++ .config/nvim/lua/snippets/mail.lua
@@ -2,7 +2,7 @@ return {
 	s("Thanks", t({ "Thanks,", "-\tRicardo.", "", "" })),
 	s("Thanks for reviewing", t({ "Thanks for reviewing,", "-\tRicardo.", "", "" })),
 	s("Thank you", t({ "Thank you,", "-\tRicardo.", "", "" })),
-	s("Regards", t({ "Best regards,", "-\tRicardo.", "", "" })),
+	s("Best regards", t({ "Best regards,", "-\tRicardo.", "", "" })),
 	s("All the best", t({ "All the best,", "-\tRicardo.", "", "" })),
 	s(
 		"Sign",
