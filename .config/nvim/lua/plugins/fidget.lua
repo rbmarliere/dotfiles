4401935a7b7eec068f6ff9f022411ffee4e33ca9
Ricardo B. Marliere <ricardo@marliere.net>
2024-06-24 01:08:07 +0000

    nvim: fidget can't be lazy

--- .config/nvim/lua/plugins/fidget.lua
+++ .config/nvim/lua/plugins/fidget.lua
@@ -1,11 +1,15 @@
 return {
 	"j-hui/fidget.nvim",
+	lazy = false,
 	opts = {
 		notification = {
 			window = {
 				normal_hl = "NormalFloat",
-				winblend = 0
+				winblend = 0,
 			},
 		},
-	}
+	},
+	keys = {
+		{ "<Leader><Leader>", ":nohlsearch | Fidget clear <CR>" },
+	},
 }
