6689243c4e1204d2595677d378627895f7405c26
Ricardo B. Marliere <ricardo@marliere.net>
2024-07-27 15:21:31 -0300

    nvim: fix fugitive key bindings

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -2,8 +2,7 @@ return {
 	"tpope/vim-fugitive",
 	lazy = false,
 	keys = {
-		{ "<Leader>g", ":vert Git<CR>" },
-		{ "<Leader>G", ":tab Git<CR>:tabm 0<CR>" },
-		{ "<Leader><Leader>g", ":Git blame<CR>:set number relativenumber<CR>" },
+		{ "<Leader>g", ":tab Git<CR>:tabm 0<CR>" },
+		{ "<Leader>gB", ":Git blame<CR>:set number relativenumber<CR>" },
 	},
 }
