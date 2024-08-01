0d58c42a9c307811b1ff229aea0c57fa3df9fa55
Ricardo B. Marliere <ricardo@marliere.net>
2024-08-01 15:20:12 +0200

    nvim: disable swap files

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,8 @@ vim.g.editorconfig = false
 vim.opt.exrc = true
 vim.opt.guicursor="n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 vim.opt.backupdir = os.getenv("HOME") .. "/.cache/nvim/bkp/"
-vim.opt.directory = os.getenv("HOME") .. "/.cache/nvim/swp/"
 vim.opt.undodir = os.getenv("HOME") .. "/.cache/nvim/und/"
 vim.opt.undofile = true
+
+vim.opt.swapfile = false
+-- vim.opt.directory = os.getenv("HOME") .. "/.cache/nvim/swp/"
