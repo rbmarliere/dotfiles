0d94af4ed18e4b3a39dac19d65cd143387e9f301
Ricardo B. Marliere <ricardo@marliere.net>
2024-11-09 11:14:12 -0300

    nvim: move e-mail snippets to all.lua

--- .config/nvim/lua/snippets/all.lua
+++ .config/nvim/lua/snippets/all.lua
@@ -1,1 +1,4 @@
-return {}
+return {
+	s("ricardo", t("Ricardo B. Marliere <ricardo@marliere.net>")),
+	s("rbm", t("Ricardo B. Marliere <rbm@suse.com>")),
+}
--- .config/nvim/lua/snippets/linux.lua
+++ .config/nvim/lua/snippets/linux.lua
@@ -1,6 +1,4 @@
 return {
-	s("Ricardo", t("Ricardo B. Marliere <ricardo@marliere.net>")),
-	s("rbm", t("Ricardo B. Marliere <rbm@suse.com>")),
 	s("Suggested-by", t("Suggested-by: "), i(1)),
 	s("Reviewed-by", t("Reviewed-by: "), i(1)),
 	s("Tested-by", t("Tested-by: "), i(1)),
