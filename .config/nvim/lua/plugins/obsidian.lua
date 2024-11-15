35ddbf6704abd02ec443d89bd80a34bf32c8eee7
Ricardo B. Marliere <ricardo@marliere.net>
2024-11-15 20:52:21 -0300

    introduce obsidian

---
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,108 @@
+return {
+	"epwalsh/obsidian.nvim",
+	version = "*", -- recommended, use latest release instead of latest commit
+	lazy = true,
+	ft = "markdown",
+	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+	-- event = {
+	--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+	--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+	--   -- refer to `:h file-pattern` for more examples
+	--   "BufReadPre " .. vim.fn.expand "~" .. "/notes/*.md",
+	--   "bufnewfile " .. vim.fn.expand "~" .. "/notes/*.md",
+	-- },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	keys = {
+		{ "<leader>o", ":ObsidianQuickSwitch<CR>" },
+		{ "<leader>ot", ":ObsidianToday<CR>" },
+		{ "<leader>O", ":Obsidian" },
+	},
+	init = function()
+		vim.opt.conceallevel = 1
+	end,
+	opts = {
+		workspaces = {
+			{
+				name = "notes",
+				path = "~/notes",
+			},
+			-- {
+			-- 	name = "work",
+			-- 	path = "~/vaults/work",
+			-- },
+		},
+
+		-- log_level = vim.log.levels.DEBUG,
+		disable_frontmatter = true,
+		notes_subdir = "inbox",
+		daily_notes = {
+			folder = "journal",
+			default_tags = {},
+		},
+		note_id_func = function(title)
+			-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+			-- In this case a note with the title 'My new note' will be given an ID that looks
+			-- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+			local suffix = ""
+			-- if title ~= nil then
+			-- 	-- If title is given, transform it into valid file name.
+			-- 	suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+			-- else
+			if title == nil then
+				-- If title is nil, just add 4 random uppercase letters to the suffix.
+				for _ = 1, 4 do
+					suffix = suffix .. string.char(math.random(65, 90))
+				end
+			end
+			return title
+			-- return tostring(os.date("%Y-%m-%d")) .. "-" .. suffix
+		end,
+		attachments = {
+			img_folder = "assets",
+		},
+		ui = {
+			enable = true, -- set to false to disable all additional syntax features
+			update_debounce = 200, -- update delay after a text change (in milliseconds)
+			max_file_length = 5000, -- disable UI features for files with more than this many lines
+			-- Define how various check-boxes are displayed
+			checkboxes = {
+				-- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+				[" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+				["x"] = { char = "", hl_group = "ObsidianDone" },
+				[">"] = { char = "", hl_group = "ObsidianRightArrow" },
+				["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+				["!"] = { char = "", hl_group = "ObsidianImportant" },
+				-- Replace the above with this if you don't have a patched font:
+				-- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+				-- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+
+				-- You can also add more custom ones...
+			},
+			-- Use bullet marks for non-checkbox lists.
+			bullets = { char = "•", hl_group = "ObsidianBullet" },
+			external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+			-- Replace the above with this if you don't have a patched font:
+			-- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+			reference_text = { hl_group = "ObsidianRefText" },
+			highlight_text = { hl_group = "ObsidianHighlightText" },
+			tags = { hl_group = "ObsidianTag" },
+			block_ids = { hl_group = "ObsidianBlockID" },
+			hl_groups = {
+				-- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+				ObsidianTodo = { bold = true, fg = "#f78c6c" },
+				ObsidianDone = { bold = true, fg = "#89ddff" },
+				ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+				ObsidianTilde = { bold = true, fg = "#ff5370" },
+				ObsidianImportant = { bold = true, fg = "#d73128" },
+				ObsidianBullet = { bold = true, fg = "#89ddff" },
+				ObsidianRefText = { underline = true, fg = "#c792ea" },
+				ObsidianExtLinkIcon = { fg = "#c792ea" },
+				ObsidianTag = { italic = true, fg = "#89ddff" },
+				ObsidianBlockID = { italic = true, fg = "#89ddff" },
+				ObsidianHighlightText = { bg = "#75662e" },
+			},
+		},
+	},
+}
