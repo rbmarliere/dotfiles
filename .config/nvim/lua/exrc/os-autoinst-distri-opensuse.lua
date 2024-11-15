a7b6045337cc2f7102fd0631f40d9f703ab97405
Ricardo B. Marliere <ricardo@marliere.net>
2024-11-15 20:59:50 -0300

    nvim: fix formatting in osado exrc

--- .config/nvim/lua/exrc/os-autoinst-distri-opensuse.lua
+++ .config/nvim/lua/exrc/os-autoinst-distri-opensuse.lua
@@ -22,8 +22,8 @@ require("conform").formatters.yamltidy = {
 }
 --
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "gitcommit",
-    callback = function()
-        vim.opt_local.colorcolumn = "72"
-    end,
+	pattern = "gitcommit",
+	callback = function()
+		vim.opt_local.colorcolumn = "72"
+	end,
 })
