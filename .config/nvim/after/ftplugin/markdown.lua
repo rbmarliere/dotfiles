c144dc3383186994fb7eedd4f41fccdde1e904a2
Ricardo B. Marliere <ricardo@marliere.net>
2024-11-18 20:46:51 -0300

    nvim: add linebreak for .md and .txt

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,6 +1,7 @@
 vim.opt_local.spell = true
 vim.opt_local.tw = 88
 vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
 vim.opt_local.smartindent = false
 vim.opt_local.expandtab = true
 vim.opt_local.shiftwidth = 2
--- .config/nvim/after/ftplugin/text.lua
+++ .config/nvim/after/ftplugin/text.lua
@@ -4,3 +4,4 @@ vim.opt_local.smartindent = false
 vim.opt_local.spell = true
 vim.opt_local.tw = 72
 vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
