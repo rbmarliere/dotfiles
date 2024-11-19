b3ec813ed3d66747acff6306c70c7ed9abcbbced
Ricardo B. Marliere <ricardo@marliere.net>
2024-11-19 08:24:30 -0300

    nvim: set spelllang maps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -11,3 +11,5 @@ vim.keymap.set("n", "<Leader>qr", ":source " .. vim.fn.stdpath("data") .. "/qf/"
 vim.keymap.set("n", "<Leader>qd", ":!rm " .. vim.fn.stdpath("data") .. "/qf/", opts)
 vim.keymap.set("n", "<Leader>q", ":ToggleQf<CR>", opts)
 vim.keymap.set("n", "<Leader>Q", ":cexpr []<CR>:cclose<CR>", opts)
+vim.keymap.set("n", "<Leader>pt", ":set spelllang=pt_br<CR>", opts)
+vim.keymap.set("n", "<Leader>en", ":set spelllang=en<CR>", opts)
