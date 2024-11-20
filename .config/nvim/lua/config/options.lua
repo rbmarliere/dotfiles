8b0ea707cfdd2972877bae2360ffb439e54af1d5
Ricardo B. Marliere <ricardo@marliere.net>
2024-11-20 09:45:15 -0300

    nvim: re-enable editorconfig

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,4 +1,4 @@
-vim.g.editorconfig = false
+vim.g.editorconfig = true
 vim.opt.exrc = true
 vim.opt.guicursor="n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 vim.opt.backupdir = os.getenv("HOME") .. "/.cache/nvim/bkp/"
