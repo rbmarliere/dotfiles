e0a138b6b95ceb9d085cc0e720ba389baca2e600
Ricardo B. Marliere <ricardo@marliere.net>
2024-12-06 11:22:14 -0300

    nvim: add visual :GBrowse keymap for vim-fugitive

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -14,7 +14,7 @@ return {
 	keys = {
 		{ "<Leader>g", ":tab Git<CR>:tabm 0<CR>" },
 		{ "<Leader>gb", ":Git blame<CR>:set number relativenumber<CR>" },
-		{ "<Leader>gB", ":GBrowse<CR>" }, -- open URL at blob (can be file, tree, commit, ...)
+		{ "<Leader>gB", ":GBrowse<CR>", mode = { "n", "v" } }, -- open URL at blob (can be file, tree, commit, ...)
 		{ "<Leader>gl", ":Git log --oneline<CR>" },
 		{ "<Leader>gL", ":GcLog %<CR>" }, -- add file history to quickfix
 		{ "<Leader>gD", ":Gdiffsplit!<CR>" }, -- solve merge conflicts (use d2o and d3o)
