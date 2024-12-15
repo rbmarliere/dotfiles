73f94473de68d5b57dab5109558acbe25209d216
Ricardo B. Marliere <ricardo@marliere.net>
2024-12-15 11:08:13 -0300

    nvim: add <c-k> to expand snippet for luasnip

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -10,5 +10,10 @@ return {
 		})
 		ls.filetype_extend("mail", { "linux" })
 		ls.filetype_extend("gitcommit", { "linux" })
+		vim.keymap.set({ "i", "s" }, "<C-k>", function()
+			if ls.expand_or_jumpable() then
+				ls.expand_or_jump()
+			end
+		end, { silent = true })
 	end,
 }
