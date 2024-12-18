f7e6fe31354be34b327fcb1bad63942f22f85294
Ricardo B. Marliere <ricardo@marliere.net>
2024-12-18 07:59:47 -0300

    nvim: use oil instead of file_browser telescope extension

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,59 @@
+return {
+	"stevearc/oil.nvim",
+	opts = {
+		-- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
+		-- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
+		-- Additionally, if it is a string that matches "actions.<name>",
+		-- it will use the mapping at require("oil.actions").<name>
+		-- Set to `false` to remove a keymap
+		-- See :help oil-actions for a list of all available actions
+		keymaps = {
+			--   ["g?"] = { "actions.show_help", mode = "n" },
+			--   ["<CR>"] = "actions.select",
+			--   ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+			--   ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+			--   ["<C-t>"] = { "actions.select", opts = { tab = true } },
+			["<C-p>"] = { "actions.preview", opts = { horizontal = true, split = "belowright" } },
+			--   ["<C-c>"] = { "actions.close", mode = "n" },
+			--   ["<C-l>"] = "actions.refresh",
+			--   ["-"] = { "actions.parent", mode = "n" },
+			--   ["_"] = { "actions.open_cwd", mode = "n" },
+			--   ["`"] = { "actions.cd", mode = "n" },
+			--   ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+			--   ["gs"] = { "actions.change_sort", mode = "n" },
+			--   ["gx"] = "actions.open_external",
+			--   ["g."] = { "actions.toggle_hidden", mode = "n" },
+			--   ["g\\"] = { "actions.toggle_trash", mode = "n" },
+		},
+	},
+	keys = {
+		{ "-", "<Cmd>Oil<CR>" },
+	},
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function(_, opts)
+		local oil = require("oil")
+
+		local grep_in_dir = function()
+			local search_dirs = oil.get_current_dir()
+			require("telescope").extensions.live_grep_args.live_grep_args({
+				search_dirs = { search_dirs },
+			})
+		end
+		opts.keymaps["<M-g>"] = { grep_in_dir }
+
+		-- https://github.com/stevearc/oil.nvim/blob/master/doc/recipes.md#toggle-file-detail-view
+		opts.keymaps["gd"] = {
+			desc = "Toggle file detail view",
+			callback = function()
+				detail = not detail
+				if detail then
+					oil.set_columns({ "icon", "permissions", "size", "mtime" })
+				else
+					oil.set_columns({ "icon" })
+				end
+			end,
+		}
+
+		oil.setup(opts)
+	end,
+}
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -15,7 +15,6 @@ return {
 	dependencies = {
 		"keyvchan/telescope-find-pickers.nvim",
 		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope-file-browser.nvim",
 		"nvim-telescope/telescope-live-grep-args.nvim",
 		"rbmarliere/telescope-cscope.nvim",
 		"tsakirist/telescope-lazy.nvim",
@@ -26,19 +25,12 @@ return {
 		{ "<C-(>", ":Telescope lsp_document_symbols<CR>" },
 		{ "<M-p>", ":Telescope find_files<CR>" },
 		{ "<C-p>", ":Telescope git_files<CR>" },
-		{ "-", ":Telescope file_browser path=%:p:h<CR>" },
 		{ "<M-g>", ":Telescope live_grep_args<CR>" },
 		{ "<Leader>gw", ":Telescope git_worktree<CR>" },
 	},
 	config = function()
 		local telescope = require("telescope")
 		local builtin = require("telescope.builtin")
-		local action_state = require("telescope.actions.state")
-		local actions = require("telescope.actions")
-		local fb_utils = require("telescope._extensions.file_browser.utils")
-		local fb_actions = require("telescope._extensions.file_browser.actions")
-		local Path = require("plenary.path")
-
 		local picker_config = {}
 		for b, _ in pairs(builtin) do
 			picker_config[b] = {
@@ -48,40 +40,6 @@ return {
 				symbol_width = 50,
 				symbol_type_width = 15,
 			}
-		end
-
-		-- https://github.com/nvim-telescope/telescope-file-browser.nvim/wiki/Configuration-Recipes
-		local path_toggle = function(prompt_bufnr)
-			local current_picker = action_state.get_current_picker(prompt_bufnr)
-			local finder = current_picker.finder
-			local bufr_path = Path:new(vim.fn.expand("#:p"))
-			local bufr_parent_path = bufr_path:parent():absolute()
-			if finder.path ~= bufr_parent_path then
-				finder.path = bufr_parent_path
-				fb_utils.selection_callback(current_picker, bufr_path:absolute())
-			else
-				finder.path = vim.loop.cwd()
-			end
-			fb_utils.redraw_border_title(current_picker)
-			current_picker:refresh(finder, {
-				new_prefix = fb_utils.relative_path_prefix(finder),
-				reset_prompt = true,
-				multi = current_picker._multi,
-			})
-		end
-		local grep_in_dir = function(prompt_bufnr)
-			local selections = fb_utils.get_selected_files(prompt_bufnr, false)
-			local search_dirs = vim.tbl_map(function(path)
-				return path:absolute()
-			end, selections)
-			if vim.tbl_isempty(search_dirs) then
-				local current_finder = action_state.get_current_picker(prompt_bufnr).finder
-				search_dirs = { current_finder.path }
-			end
-			actions.close(prompt_bufnr)
-			require("telescope").extensions.live_grep_args.live_grep_args({
-				search_dirs = search_dirs,
-			})
 		end
 
 		local opts = {
@@ -161,56 +119,10 @@ return {
 						},
 					},
 				},
-				file_browser = {
-					-- https://github.com/nvim-telescope/telescope.nvim/issues/2779
-					temp__scrolling_limit = 1000,
-					-- layout_strategy = "flex",
-					-- layout_config = {
-					-- 	vertical = {
-					-- 		preview_height = 0,
-					-- 	},
-					-- },
-					grouped = true,
-					prompt_path = true,
-					git_status = false,
-					respect_gitignore = false,
-					hidden = { file_browser = true, folder_browser = true },
-					hide_parent_dir = true,
-					follow_symlinks = true,
-					hijack_netrw = true,
-					mappings = {
-						i = {
-							["<C-t>"] = actions.select_tab, -- https://github.com/nvim-telescope/telescope-file-browser.nvim/issues/250
-							["<M-g>"] = grep_in_dir,
-							["<C-e>"] = path_toggle,
-							["<C-i>"] = fb_actions.toggle_respect_gitignore,
-						},
-						n = {
-							["-"] = fb_actions.goto_parent_dir,
-							t = actions.select_tab,
-							T = fb_actions.change_cwd,
-							["<M-g>"] = grep_in_dir,
-							["<C-e>"] = path_toggle,
-							["<C-i>"] = fb_actions.toggle_respect_gitignore,
-							["<C-p>"] = actions.move_selection_previous,
-							["<C-n>"] = actions.move_selection_next,
-						},
-					},
-				},
 			},
 		}
 
-		vim.keymap.set("v", "<M-g>", function()
-			vim.cmd('normal! "vy')
-			local visual_selection = vim.fn.getreg("v"):gsub("\n", "")
-			telescope.extensions.live_grep_args.live_grep_args({
-				default_text = visual_selection,
-			})
-		end, { noremap = true, silent = true })
-
 		telescope.setup(opts)
-		-- telescope.load_extension("cscope")
-		telescope.load_extension("file_browser")
 		telescope.load_extension("find_pickers")
 		telescope.load_extension("fzf")
 		telescope.load_extension("live_grep_args")
