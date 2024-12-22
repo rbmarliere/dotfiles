685ac2fee7f91abd0a93431f76d87658af9e3d87
Ricardo B. Marliere <ricardo@marliere.net>
2024-12-22 20:04:33 -0300

    nvim: no need for nvim-snippets

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,54 +1,6 @@
 return {
 	"saghen/blink.cmp",
 	version = "v0.*",
-	dependencies = {
-		{
-			"garymjr/nvim-snippets",
-			keys = {
-				{
-					"<Tab>",
-					function()
-						if vim.snippet.active({ direction = 1 }) then
-							vim.schedule(function()
-								vim.snippet.jump(1)
-							end)
-							return
-						end
-						return "<Tab>"
-					end,
-					expr = true,
-					silent = true,
-					mode = "i",
-				},
-				{
-					"<Tab>",
-					function()
-						vim.schedule(function()
-							vim.snippet.jump(1)
-						end)
-					end,
-					expr = true,
-					silent = true,
-					mode = "s",
-				},
-				{
-					"<S-Tab>",
-					function()
-						if vim.snippet.active({ direction = -1 }) then
-							vim.schedule(function()
-								vim.snippet.jump(-1)
-							end)
-							return
-						end
-						return "<S-Tab>"
-					end,
-					expr = true,
-					silent = true,
-					mode = { "i", "s" },
-				},
-			},
-		},
-	},
 	opts = {
 		keymap = { preset = "default" },
 		signature = { enabled = true },
@@ -57,9 +9,25 @@ return {
 			nerd_font_variant = "mono",
 		},
 		completion = {
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 500,
+			},
 			menu = {
 				-- ExtraWhiteSpace is red in alabaster theme, disable it here
 				winhighlight = "Normal:BlinkCmpMenu,FloatBorder:BlinkCmpMenuBorder,CursorLine:BlinkCmpMenuSelection,Search:None,ExtraWhiteSpace:Normal",
+			},
+		},
+		sources = {
+			providers = {
+				snippets = {
+					opts = {
+						search_paths = {
+							vim.fn.stdpath("config") .. "/snippets",
+							"snippets", -- relative path, per-project
+						},
+					},
+				},
 			},
 		},
 	},
